371461521510d2d3ab8219f4b4113988674f1a37
Kristen McCabe <kmccabe3@gmail.com>
2014-12-03 11:18:11 -0800

    add sign up link & matching route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get  "users",      to: "users#index",   as: :users
-  get  "users/new",  to: "users#new"
+  get  "users/new",  to: "users#new",     as: :new_user
   post "users",      to: "users#create"
 
   root "home#index"
