bf289469e7219bdd3475a763d762f40c3860ddb6
Kristen McCabe <kmccabe3@gmail.com>
2014-12-02 16:53:52 -0800

    add two passing tests for users#new

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, :type => :controller do
+
+  describe "GET #new" do
+    it "is successful" do
+      get :new
+      expect(response.status).to eq 200
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+end
