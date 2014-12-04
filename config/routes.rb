b72435c862dc091383139dfa2d62b6cc5bb25e50
Kristen McCabe <kmccabe3@gmail.com>
2014-12-04 12:10:18 -0800

    add route for posts#create

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # Posts Routes
   get  "posts",      to: "posts#index",      as: :posts
   get  "posts/new",  to: "posts#new",        as: :new_post
+  post "posts",      to: "posts#create"
 
   root "home#index"
 
