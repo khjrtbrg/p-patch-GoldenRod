62c37efcee256afcf6d580ba7416ec10505e9c6e
Kristen McCabe <kmccabe3@gmail.com>
2014-12-04 09:55:18 -0800

    refactor create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if find_user && authenticate_password
+    if find_user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
@@ -21,12 +22,4 @@ class SessionsController < ApplicationController
   def find_user
     @user = User.find_by(email: params[:email])
   end
-
-  def authenticate_password
-    if @user.password == params[:password]
-      session[:user_id] = @user.id
-      # @user
-    end
-  end
-
 end
