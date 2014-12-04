528d5799380d71a51868475f4fb484ca38dfe6b1
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-04 11:20:19 -0800

    user redirected from new if not logged in or admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
+  before_action :admin?
+
   def index
+
+  end
+
+  def new
+    redirect_to posts_path unless @admin
+  end
+
+  private
+
+  def admin?
     @admin = current_user.admin if logged_in?
   end
 end
