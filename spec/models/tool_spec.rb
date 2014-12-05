1326b12d6e30b6a5519fdafd21b40148a3b4bd93
Kristen McCabe <kmccabe3@gmail.com>
2014-12-05 11:12:15 -0800

    add non-passing test for http validation

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Tool, :type => :model do
     it "is valid" do
       tool = Tool.create(
         name: "Shovel",
-        image_url: "http://www.tools.com/tools/shovel.jpg",
+        image_url: "http://www.homedepot.com/catalog/productImages/300/07/078a19f9-63ca-469b-bc27-6982bd6a4ce6_300.jpg",
         user_id: admin.id
       )
       expect(tool.valid?).to eq true
@@ -22,7 +22,7 @@ RSpec.describe Tool, :type => :model do
 
     it "is invalid without name" do
       tool = Tool.create(
-        image_url: "http://www.tools.com/tools/shovel.jpg",
+        image_url: "http://www.homedepot.com/catalog/productImages/300/07/078a19f9-63ca-469b-bc27-6982bd6a4ce6_300.jpg",
         user_id: admin.id
       )
       expect(tool.valid?).to eq false
@@ -39,9 +39,21 @@ RSpec.describe Tool, :type => :model do
     it "is invalid without user_id" do
       tool = Tool.create(
         name: "Shovel",
-        image_url: "http://www.tools.com/tools/shovel.jpg",
+        image_url: "http://www.homedepot.com/catalog/productImages/300/07/078a19f9-63ca-469b-bc27-6982bd6a4ce6_300.jpg",
       )
       expect(tool.valid?).to eq false
+    end
+
+    it "is invalid if image_url is not web address" do
+      tool = Tool.create(
+        name: "Shovel",
+        user_id: admin.id,
+        image_url: "foo"
+      )
+      expect(tool.valid?).to eq false
+    end
+
+    it "is invalid if image_url is not valid web address" do
     end
   end
 end
