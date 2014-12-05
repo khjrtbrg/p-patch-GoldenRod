dc8f183867e50db526acd6ef3db433e647cbb934
Kristen McCabe <kmccabe3@gmail.com>
2014-12-04 22:05:53 -0800

    format post date

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,7 @@ class Post < ActiveRecord::Base
   # Validations
   validates :title, :content, :user_id, presence: true
 
+  def pretty_updated_at
+    updated_at.strftime("%m/%d/%y, %T")
+  end
 end
