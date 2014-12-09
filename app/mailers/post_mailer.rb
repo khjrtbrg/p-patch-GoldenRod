cb36e41b0fe105944dd790d83c0df0fea7abae11
Kristen McCabe <kmccabe3@gmail.com>
2014-12-09 15:30:49 -0800

    add new_post_alert(user, post)

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,3 +1,9 @@
 class PostMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def new_post_alert(user, post)
+    @user    = user # this scopes to the mailer view
+    @post_id = post.id
+    mail(to: @user.email, subject: "News @ Tendril: #{post.title}" )
+  end
 end
