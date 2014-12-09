8f49272d648090afdd3e204d50879c8495bd568c
Kristen McCabe <kmccabe3@gmail.com>
2014-12-09 11:26:48 -0800

    add route for posts#show

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get  "posts",      to: "posts#index",      as: :posts
   get  "posts/new",  to: "posts#new",        as: :new_post
   post "posts",      to: "posts#create"
+  get  "posts/:id",  to: "posts#show",       as: :post
 
   # Tools Routes
   get  "tools",      to: "tools#index",      as: :tools
