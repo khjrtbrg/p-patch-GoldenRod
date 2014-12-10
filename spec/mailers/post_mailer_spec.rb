3c29a271dc6ec03cd07de1ed219f0ea0d7194414
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-09 16:39:51 -0800

    first test for mailer spec

--- spec/mailers/post_mailer_spec.rb
+++ spec/mailers/post_mailer_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe PostMailer, :type => :mailer do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#new_post_alert" do
+    let!(:post) { create(:post) }
+    let!(:user) { create(:user) }
+
+    before(:each) do
+      ActionMailer::Base.delivery_method = :test
+      ActionMailer::Base.perform_deliveries = true
+      ActionMailer::Base.deliveries = []
+      PostMailer.new_post_alert(user, post).deliver
+    end
+
+    after(:each) do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it "successfully sends" do
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
+  end
 end
