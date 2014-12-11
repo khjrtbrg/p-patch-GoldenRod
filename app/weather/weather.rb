a931a15882a05687f5c61728909148f1f2d0401e
Kristen McCabe <kmccabe3@gmail.com>
2014-12-10 17:11:32 -0800

    move forecast logic into forecast class

--- app/weather/weather.rb
+++ app/weather/weather.rb
@@ -21,18 +21,4 @@ class Weather
   def convert_temp_to_f(temp)
     ((temp - 273.15) * 1.8000 + 32.00).to_i
   end
-
-  def self.forecast
-    # get array of forecasts
-    forecast = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=Seattle,us").parsed_response
-    forecast_details = forecast["list"]
-    forecast_details.select do |time|
-      time["dt_txt"].match(/12:00:00/)
-    end
-
-    # each loop through array
-    # creating Weather objects for each day
-    # return array of weather objects
-  end
-
 end
