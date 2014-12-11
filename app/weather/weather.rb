943843e1e7a294d3f17cbdc9bc0762b90ac1b137
Kristen McCabe <kmccabe3@gmail.com>
2014-12-11 11:17:02 -0800

    finalize current_weather + forecast methods

--- app/weather/weather.rb
+++ app/weather/weather.rb
@@ -1,21 +1,30 @@
 class Weather
   attr_accessor :description, :icon, :humidity, :temp
 
-  def initialize
-    weather      = current_weather
+  def initialize(weather_data)
+    weather      = weather_data
+    @date_time   = Time.at(weather["dt"])
     @description = weather["weather"][0]["description"]
     @icon        = "http://openweathermap.org/img/w/#{weather['weather'][0]['icon']}.png"
     @humidity    = weather["main"]["humidity"]
     @temp        = convert_temp_to_f(weather["main"]["temp"])
   end
 
-  # Returns Seattle weather as JSON
-  def request(path)
+  def self.request(path)
     HTTParty.get("http://api.openweathermap.org/data/2.5/#{path}?q=Seattle,us").parsed_response
   end
 
-  def current_weather
-    request("weather")
+  def self.current_weather
+    weather_hash = request("weather")
+    Weather.new(weather_hash)
+  end
+
+  def self.forecast
+    forecast_data  = request("forecast")["list"]
+    forecasts = forecast_data.select do |weather_hash|
+      weather_hash["dt_txt"].match(/12:00:00/)
+    end
+    forecasts.map { |weather_hash| Weather.new(weather_hash) }
   end
 
   def convert_temp_to_f(temp)
