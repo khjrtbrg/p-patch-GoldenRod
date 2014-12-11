0f2bd2f4db2157d7fd9bfd6c2e69d2a4c66bbea8
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-10 16:45:09 -0800

    starting to work on forecast

--- app/weather/weather.rb
+++ app/weather/weather.rb
@@ -22,17 +22,17 @@ class Weather
     ((temp - 273.15) * 1.8000 + 32.00).to_i
   end
 
-  ## forecast ideas?
+  def self.forecast
+    # get array of forecasts
+    forecast = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=Seattle,us").parsed_response
+    forecast_details = forecast["list"]
+    forecast_details.select do |time|
+      time["dt_txt"].match(/12:00:00/)
+    end
 
-  # def forecast
-  #   request("forecast")
-  # end
-
-  # def self.forecast
-  #   # get array of forecasts
-  #   # each loop through array
-  #   # creating Weather objects for each day
-  #   # return array of weather objects
-  # end
+    # each loop through array
+    # creating Weather objects for each day
+    # return array of weather objects
+  end
 
 end
