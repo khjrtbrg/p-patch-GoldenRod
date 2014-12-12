28c5dd62b19d98aab695a5628b67d5e78a84c6f6
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-12 15:54:35 -0800

    fiddling with format

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -9,12 +9,21 @@ module CalendarHelper
   end
 
   def generate_month_days(events)
+    days = event_days(events)
     string = ""
     counter = 1
     Event.days_in_month.times do
-      string << "<div class='day'><span>#{counter}</span></div>\n"
+      if days.include?(counter)
+        string << "<div class='day'><span>#{counter}</span><p>Events Today!</p></div>"
+      else
+        string << "<div class='day'><span>#{counter}</span></div>"
+      end
       counter += 1
     end
     string.html_safe
+  end
+
+  def event_days(events)
+    events.pluck(:date).map { |date| date.day }
   end
 end
