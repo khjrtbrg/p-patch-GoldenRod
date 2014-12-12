2dbb1de244a87c7c45e882865d37c17f672c5777
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-12 11:03:57 -0800

    adding find_user before action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,15 @@
 class EventsController < ApplicationController
+  before_action :find_user
+
   def index
+  end
+
+  def new
+  end
+
+  private
+
+  def find_user
+    @user = current_user if logged_in?
   end
 end
