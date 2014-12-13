af6bb78fb0712303fa32222e4311d11bc3a02f1e
Kristen McCabe <kmccabe3@gmail.com>
2014-12-12 16:46:30 -0800

    change order of methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,16 +6,16 @@ class Event < ActiveRecord::Base
   validates :title, :date, :user_id, presence: true
 
   # Methods
-  def self.events_this_month
-    Event.where(date: (start_month..end_month))
-  end
-
   def self.start_month
     Date.today.at_beginning_of_month
   end
 
   def self.end_month
     Date.today.at_end_of_month
+  end
+
+  def self.events_this_month
+    Event.where(date: (start_month..end_month))
   end
 
   def self.days_in_month
