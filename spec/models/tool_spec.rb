3cb37fffa380ffa780ea9fcd81b1f358a1b3a4a9
Kristen McCabe <kmccabe3@gmail.com>
2014-12-14 17:43:05 -0800

    update tests w/ factory_girl

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -2,42 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Tool, :type => :model do
 
+  let!(:tool) { create(:tool) }
+
   describe ".create" do
     it "is valid" do
-      tool = Tool.create(
-        name: "Shovel",
-        image_url: "http://www.homedepot.com/catalog/productImages/300/07/078a19f9-63ca-469b-bc27-6982bd6a4ce6_300.jpg"
-      )
       expect(tool.valid?).to eq true
     end
 
     it "is invalid without name" do
-      tool = Tool.create(
-        image_url: "http://www.homedepot.com/catalog/productImages/300/07/078a19f9-63ca-469b-bc27-6982bd6a4ce6_300.jpg"
-      )
-      expect(tool.valid?).to eq false
+      invalid_tool = build(:tool, name: "")
+      expect(invalid_tool.invalid?).to eq true
     end
 
     it "is invalid without image_url" do
-      tool = Tool.create(
-        name: "Shovel"
-      )
-      expect(tool.valid?).to eq false
+      invalid_tool = build(:tool, image_url: "")
+      expect(invalid_tool.invalid?).to eq true
     end
 
     it "is invalid if image_url is not web address" do
-      tool = Tool.create(
-        name: "Shovel",
-        image_url: "foo"
-      )
-      expect(tool.valid?).to eq false
+      invalid_tool = build(:tool, image_url: "foo")
+      expect(invalid_tool.invalid?).to eq true
     end
 
     # it "is invalid if image_url is not valid web address" do
-    #   tool = Tool.create(
-    #     name: "Shovel",
-    #     image_url: "http://tools.com/tool.jpg"
-    #   )
+    #   tool = build(:tool, image_url: "http://tools.com/tool.jpg")
     #   expect(tool.valid?).to eq false
     # end
   end
@@ -73,7 +61,7 @@ RSpec.describe Tool, :type => :model do
     end
 
     it "returns false if not checked out" do
-      expect(tool.unavailable?).to eq false 
+      expect(tool.unavailable?).to eq false
     end
   end
 end
