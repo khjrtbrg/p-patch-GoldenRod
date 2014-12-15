62163c4260f4dfde1991f7f709edf0293ece7d00
Kristen McCabe <kmccabe3@gmail.com>
2014-12-14 18:03:50 -0800

    clean up tests a little

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -9,24 +9,26 @@ RSpec.describe Event, :type => :model do
       expect(event.valid?).to eq true
     end
 
-    it "is invalid without title" do
-      invalid_event = build(:event, title: "") # this will fail w/ create
-      expect(invalid_event.invalid?).to eq true
-    end
+    context "is invalid" do
+      it "without title" do
+        invalid_event = build(:event, title: "") # this will fail w/ create
+        expect(invalid_event.invalid?).to eq true
+      end
 
-    it "is invalid without date" do
-      invalid_event = build(:event, date: "") # this will fail w/ create
-      expect(invalid_event.invalid?).to eq true
-    end
+      it "without date" do
+        invalid_event = build(:event, date: "")
+        expect(invalid_event.invalid?).to eq true
+      end
 
-    it "is invalid with improperly formatted date" do
-      invalid_event = build(:event, date: "fish") # this will fail w/ create
-      expect(invalid_event.invalid?).to eq true
-    end
+      it "with improperly formatted date" do
+        invalid_event = build(:event, date: "fish")
+        expect(invalid_event.invalid?).to eq true
+      end
 
-    it "is invalid without user_id" do
-      invalid_event = build(:event, user_id: "") # this will fail w/ create
-      expect(invalid_event.invalid?).to eq true
+      it "without user_id" do
+        invalid_event = build(:event, user_id: "")
+        expect(invalid_event.invalid?).to eq true
+      end
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,47 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  describe "Post.create" do
-    let(:admin) { User.create( # creates a functional variable 'user'
-      user_name: "kristen",
-      email: "k@m.com",
-      password: "hello",
-      password_confirmation: "hello",
-      admin: true
-    )}
 
+  let!(:post) { create(:post) }
+
+  describe ".create" do
     it "is valid" do
-      post = Post.create(
-        title: "Foo",
-        content: "Bar",
-        user_id: admin.id
-      )
       expect(post.valid?).to eq true
     end
 
-    it "is invalid without title" do
-      post = Post.create(
-        content: "Bar",
-        user_id: admin.id
-      )
-      expect(post.valid?).to eq false
-    end
+    context "is invalid" do
+      it "without title" do
+        invalid_post = build(:post, title: "")
+        expect(invalid_post.invalid?).to eq true
+      end
 
-    it "is invalid without content" do
-      post = Post.create(
-        title: "Foo",
-        user_id: admin.id
-      )
-      expect(post.valid?).to eq false
-    end
+      it "without content" do
+        invalid_post = build(:post, content: "")
+        expect(invalid_post.invalid?).to eq true
+      end
 
-    it "is invalid without user_id" do
-      post = Post.create(
-        title: "Foo",
-        content: "Bar",
-      )
-      expect(post.valid?).to eq false
+      it "without user_id" do
+        invalid_post = build(:post, user_id: "")
+        expect(invalid_post.invalid?).to eq true
+      end
     end
-
   end
 end
