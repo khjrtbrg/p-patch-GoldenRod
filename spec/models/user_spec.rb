b12e410ac39a101942ffaeb0f21a231e5d83d077
Kristen McCabe <kmccabe3@gmail.com>
2014-12-14 18:06:14 -0800

    add context

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,60 +4,62 @@ describe User, :type => :model do
 
   let!(:user) { create(:user) }
 
-  describe "User.create" do
+  describe ".create" do
     it "is valid" do
       expect(user.valid?).to eq true
     end
 
-    it "is invalid without username" do
-      invalid_user = build(:user, user_name: "")
-      expect(invalid_user.invalid?).to eq true
-    end
+    context "is invalid" do
+      it "without username" do
+        invalid_user = build(:user, user_name: "")
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid with username < 7 characters" do
-      invalid_user = build(:user, user_name: "K")
-      expect(invalid_user.invalid?).to eq true
-    end
+      it "with username < 7 characters" do
+        invalid_user = build(:user, user_name: "K")
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid without email" do
-      invalid_user = build(:user, email: "")
-      expect(invalid_user.invalid?).to eq true
-    end
+      it "without email" do
+        invalid_user = build(:user, email: "")
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid without email format" do
-      invalid_user = build(:user, email: "kristen")
-      expect(invalid_user.invalid?).to eq true
-    end
+      it "without email format" do
+        invalid_user = build(:user, email: "kristen")
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid without password" do
-      invalid_user = build(:user, password: "")
-      expect(invalid_user.invalid?).to eq true
-    end
+      it "without password" do
+        invalid_user = build(:user, password: "")
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid without password confirmation" do
-      invalid_user = build(:user, password_confirmation: "")
-      expect(invalid_user.invalid?).to eq true
-    end
+      it "without password confirmation" do
+        invalid_user = build(:user, password_confirmation: "")
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid without matching password confirmation" do
-      invalid_user = build(
-        :user,
-        password: "foo",
-        password_confirmation: "bar"
-      )
-      expect(invalid_user.invalid?).to eq true
-    end
+      it "without matching password confirmation" do
+        invalid_user = build(
+          :user,
+          password: "foo",
+          password_confirmation: "bar"
+        )
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid without a unique username" do
-      build(:user, user_name: "Kristina")
-      invalid_user = build(:user, user_name: "Kristina")
-      expect(invalid_user.invalid?).to eq true
-    end
+      it "without a unique username" do
+        build(:user, user_name: "Kristina")
+        invalid_user = build(:user, user_name: "Kristina")
+        expect(invalid_user.invalid?).to eq true
+      end
 
-    it "is invalid without a unique email" do
-      build(:user, email: "k@h.com")
-      invalid_user = build(:user, email: "k@h.com")
-      expect(invalid_user.invalid?).to eq true
+      it "without a unique email" do
+        build(:user, email: "k@h.com")
+        invalid_user = build(:user, email: "k@h.com")
+        expect(invalid_user.invalid?).to eq true
+      end
     end
   end
 end
